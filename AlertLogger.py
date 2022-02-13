d57823934abde33b02101b49e71ea101622146a8
Daniel <daniel.clover@potatoesmakechips.co.uk>
2022-02-13 18:21:18 +0000

    Rules and Discord Intigration

--- AlertLogger.py
+++ AlertLogger.py
@@ -1,8 +1,59 @@
-import csv
+# Main imports
 from datetime import datetime
-def logAlert(username, alertReason):
-    fields=[datetime.utcnow(),username,alertReason]
+
+# Required for CSV logging
+import csv
+
+# Requires for Discord Webhook Logging
+import json
+import requests
+import os
+
+def logAlert(username, alertReason, message):
+    fields=[str(datetime.utcnow()),username,alertReason,message]
+
+    # CSV logging
     with open(r'alertLog.csv', 'a') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
         f.close()
+
+    # Discord Webhook Logging
+    url = os.environ['DISCORD_WEBHOOK']
+    header = {"content-type": "application/json"}
+    payload = {
+        "username": "ZuluAlpha",
+        "avatar_url": "",
+        "content": str("||<@&" + os.environ['DISCORD_MOD_ROLE_ID'] + ">||"),
+        "embeds": [
+            {
+                "author": {
+                        "name" : "ZuluAlpha Twitch Bot"
+                },
+                "title": "Twitch Mod Alert",
+                "fields": [
+                    {
+                        "name": "Time",
+                        "value": fields[0],
+                        "inline": True
+                    },
+                    {
+                        "name": "User",
+                        "value": fields[1],
+                        "inline": True
+                    },
+                    {
+                        "name": "Alert",
+                        "value": fields[2],
+                        "inline": False
+                    },
+                    {
+                        "name": "Message",
+                        "value": fields[3],
+                        "inline": False
+                    }
+                ]
+            }
+        ]
+    }
+    response= requests.post(url,data=json.dumps(payload), headers=header, verify=True)
--- Rules.py
+++ Rules.py
@@ -1,2 +1,9 @@
-def checkMessage(message):
-    pass
+import Ruleset
+import AlertLogger
+def checkMessage(author,message):
+    if Ruleset.isBadLink(message):
+        AlertLogger.logAlert(str(author),"non-clip link posted in chat",str(message))
+        return 1
+    if Ruleset.notEnglish(message):
+        AlertLogger.logAlert(str(author),"language detected as not english by cld3 with over 90% probability",str(message))
+        return 1
